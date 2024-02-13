bd51c0c8eee126c53126f433da32fc242872cefe
Dmytro <dmytro.malanchuk@elev.ga.ntig.se>
2024-02-13 08:39:15 +0100

    first commit

---
+++ gg.py
@@ -0,0 +1,54 @@
+from colors import bcolors
+
+__author__  = "Dmytro Malanchuk"
+__version__ = "1.0.0"
+__email__   = "namn.efternamn@elev.ga.ntig.se"
+
+
+
+
+# Min vänlista
+Friends_list = ["Dmytro"]
+
+def Show_list(name_lista):
+    print("======================")
+    print(bcolors.CYAN + "Vänner i listan:")
+    for name in name_lista:
+        print(bcolors.GREEN + name)
+
+# Lägga till en vän i listan
+def add_name(name_lista):
+    new_name = input("Ange vännen du vill lägga till: ")
+    name_lista.append(new_name)
+    print(f"{new_name} har lagts till i listan.")
+
+# Ta bort ett namn från listan
+def remove_name(name_lista):
+    name = input("Ange vännen du vill ta bort: ")
+    if name in name_lista:
+        name_lista.remove(name)
+        print(f"{name} har tagits bort från listan.")
+    else:
+        print(f"{name} finns inte med i listan.")
+
+while True:
+    print("******************")
+    print("\nVälj en åtgärd:")
+    print("1. Visa listan")
+    print("2. Lägg till en vän")
+    print("3. Ta bort en vän")
+    print("4. Avsluta programet")
+    print(bcolors.DEFAULT + "\n*******************")
+    
+    choice = input(bcolors.CYAN + "Vad vill du göra: ")
+    
+    if choice == "1":
+        Show_list(Friends_list)
+    elif choice == "2":
+        add_name(Friends_list)
+    elif choice == "3":
+        remove_name(Friends_list)
+    elif choice == "4":
+        break
+    else:
+        print(bcolors.RED + "Du får bara välja mella 1 och 4, var snäll och försök igen men utan dumheter den här gången.")
